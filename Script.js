fa2dd0f36d85aaaedffa2bb1b928bc3ca1ff8cc8
BabyBoy666 <83471679+BabyBoy666@users.noreply.github.com>
2022-02-03 16:59:33 +0000

    Initial commit

---
+++ Script.js
@@ -0,0 +1,62 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+   var reveals = document.querySelectorAll(".reveal2");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+function reveal2() {
+var reveals = document.querySelectorAll(".reveal3");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal2);
+
+function reveal3() {
+var reveals = document.querySelectorAll("#end");
+var one = document.querySelectorAll(".reveal");
+var two = document.querySelectorAll(".reveal2");
+var three = document.querySelectorAll(".reveal3");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      one[i].classList.add("leave");
+      two[i].classList.add("leave");
+      three[i].classList.add("leave");
+    } else {
+      one[i].classList.remove("leave");
+      two[i].classList.remove("leave");
+      three[i].classList.remove("leave");
+    }
+  }
+}
+window.addEventListener("scroll", reveal3);
